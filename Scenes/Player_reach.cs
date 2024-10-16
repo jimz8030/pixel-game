7c064605d76a6ec67886b61b77ced08230c2d5de
jasonmcgraw12 <jasonmcgraw12@byui.edu>
2024-10-16 10:07:03 -0700

    made a player's area (about within arm reach) detect when there was a tree nearby, next I need to get the two to interact and then give resources and eventually get destroyed.

--- Scenes/Player_reach.cs
+++ Scenes/Player_reach.cs
@@ -3,6 +3,18 @@ using System;
 
 public partial class Player_reach : Area2D
 {
+	
+	public void GetNearbyArea2D(Node2D Area2D){
+		if (Area2D.Name == "Sappling"){
+			GD.Print("Sapling confirmed");
+		}
+		else{
+			GD.Print("something else is afoot. also area2d name is: "+Area2D.Name);
+		}
+
+
+	}
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
--- Scenes/Sappling.cs
+++ Scenes/Sappling.cs
@@ -3,11 +3,20 @@ using System;
 using System.Collections;
 using System.Runtime.InteropServices;
 using static Godot.GD;
-
+//Jason McGraw 10/16/24
 
 public partial class Sappling : Area2D
 {
+    //bool player_nearby = false;
+    //detects when player or another phisicsbody2d enters the area
     protected void OnBodyEnter(Node2D PhysicsBody2D){
         GD.Print("Collision confirmed");
+        //player_nearby = true;
+
+    }
+    //detects when player or another phisicsbody2d leaves it's area
+    protected void OnBodyExit(Node2D PhysicsBody2D){
+        GD.Print("collision exited");
+        //player_nearby = false;
     }
 }
