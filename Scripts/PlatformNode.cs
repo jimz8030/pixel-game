ba73ecdb6de9708594666bcf0da36608a63ba511
jimz8030 <jimz8030@gmail.com>
2024-12-17 11:07:27 -0700

    Fixed Camera kinda

--- Scripts/PlatformNode.cs
+++ Scripts/PlatformNode.cs
@@ -3,15 +3,15 @@ using System;
 
 public partial class PlatformNode : StaticBody2D
 {
-    public SecondaryScript SecondaryData;
+	public SecondaryScript SecondaryData;
 	Sprite2D SecondarySprite;
 	CollisionShape2D SecondaryShape;
 
-    public override void _Ready()
-    {
+	public override void _Ready()
+	{
 		Initialize(SecondaryData);
-    }
-    public void Initialize(SecondaryScript SecondaryType){
+	}
+	public void Initialize(SecondaryScript SecondaryType){
 		SecondarySprite = GetNode<Sprite2D>("PlatformImage");
 		SecondaryShape = GetNode<CollisionShape2D>("PlatformSize");
 		//this.Name = SecondaryType.ResourceName;
