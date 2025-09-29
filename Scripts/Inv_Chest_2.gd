e5f028e52e187586dbee9e8ff3725a9bc8f46d7a
jimz8030 <jimz8030@gmail.com>
2025-09-28 18:22:54 -0600

    Revamped the Inventory

--- Scripts/Inv_Chest_2.gd
+++ Scripts/Inv_Chest_2.gd
@@ -8,33 +8,54 @@ var selected_item : Node2D = null
 @onready var line_of_sight_2: RayCast2D = $Line_of_Sight_2
 @onready var lift_line: Path2D = $Lift_Line
 
-#used to anticipate item being placed inside item frame while item is hovering over the item frame
-var stick_waiting = false
+var available_slot : Control
 
 #HANDLE ITEM DRAGGING
 func _input(event: InputEvent) -> void:
 
+	#Toggle Inventory
+	if event.is_action_pressed("Inventory"):
+		if $Slots.visible == true:
+			$Slots.visible = false
+		else:
+			$Slots.visible = true
+
 	#CHECK FOR ITEM
 	if event.is_action_pressed("Left_Click"):
-		if line_of_sight_1.get_collider() != null:
+
+		if line_of_sight_1.is_colliding():
 			if line_of_sight_1.get_collider().get_class() == "RigidBody2D":
 				selected_item = find_colliding_item()
+			elif line_of_sight_2.is_colliding():
+				if line_of_sight_2.get_collider().get_class() == "RigidBody2D":
+					selected_item = find_colliding_item()
 		elif line_of_sight_2.get_collider() != null:
 			if line_of_sight_2.get_collider().get_class() == "RigidBody2D":
 				selected_item = find_colliding_item()
+
+		#Figure out what slot the object would go to
+		for slot in $Slots.get_children():
+			#reset the available slot
+			available_slot = null
+			#search through slots for an opening
+			if slot.get_child_count() == 1:
+				available_slot = slot
+				break
 
 	if event is InputEventMouseMotion:
 		
 		#get cursor position and make a "feeler" (or the pointer) follow it
 		$Pointer.set_position(get_local_mouse_position())
-		line_of_sight_1.set_target_position((get_local_mouse_position() + Vector2(4,-34)) * 1.1)
-		line_of_sight_2.set_target_position((get_local_mouse_position() + Vector2(4,-28)) *1.1)
+		#lines are drawn near the cursor and used to determine if there's anything in the way
+		line_of_sight_1.set_target_position((get_local_mouse_position() + Vector2(-22,-32)) * 1.1)
+		line_of_sight_2.set_target_position((get_local_mouse_position() + Vector2(-22,-38)) * 1.1)
 
 		#declares that there is an item to select
 		if selected_item != null:
 
 			#ACTIVELY DRAGGING ITEM
 			if Input.is_action_pressed("Left_Click"):
+
 				#reparent item to scene (item no longer follows item frame)
 				selected_item.reparent(get_tree().get_current_scene())
 				#item is not frozen (item's rigidbody is active)
@@ -44,31 +65,17 @@ func _input(event: InputEvent) -> void:
 				#item collision changes to collide with the world
 				if selected_item.collectable or selected_item.attachable:
 					selected_item.collision_layer = 4
-					selected_item.collision_mask = 1 | 4 | 8
+					selected_item.collision_mask = 1 | 4
 				
 				#DRAW EFFORT CURVE
-				lift_line.curve.set_point_position(1, selected_item.position - $"..".position + Vector2(0,50))
+				lift_line.curve.set_point_position(1, selected_item.position - $"..".position + Vector2(22,40))
 				lift_line.curve.set_point_out(0, $Pointer.position)
-				lift_line.curve.set_point_position(0, Vector2(-2,45))
+				lift_line.curve.set_point_position(0, line_of_sight_2.position)
 
-			#DROPPED ITEM
-			elif stick_waiting == false:
-				#if no longer clicking, and item is outside the item frame, item drops.
+			#item drops if there are no slots
+			else:
 				selected_item.get_child(0).set_node_b(selected_item.get_path())
 				selected_item = null
-				lift_line.curve.set_point_position(1, Vector2(0, 41))
-
-			#ITEM PLACED INSIDE ITEM FRAME
-			elif stick_waiting == true:
-				#item's collisions are set to no longer collide with the map
-				selected_item.collision_layer = 8
-				selected_item.collision_mask = 8
-				#call_deferred performs code when it can, so the rigidbody doesn't break. Other than that, I don't know
-				selected_item.freeze = true
-				#item is reparented to the item frame (item follows item frame)
-				selected_item.reparent($Items)
-				#after code is ran, the item frame no longer anticipates an item being placed inside.
-				stick_waiting = false
 				lift_line.curve.set_point_position(1, Vector2(0, 41))
 
 			if selected_item != null:
@@ -78,48 +85,52 @@ func _input(event: InputEvent) -> void:
 					lift_line.curve.set_point_position(1, Vector2(0, 41))
 	queue_redraw()
 
-	#EAT OR EQUIP ITEM
-	#ITEM IN FRAME
-	if event.is_action_pressed("Use") and selected_item == null:
-		if $Items.get_child_count() > 0:
-			#EAT ITEM
-			if $Items.get_child(0).eat_heal_amount > 0:
-				get_parent().current_health += $Items.get_child(0).eat_heal_amount
+	#STORE ITEM
+	if event.is_action_pressed("Store") and selected_item != null:
+		if selected_item.eat_heal_amount > 0 or selected_item.equipable:
+			if available_slot != null:
+				#item's collisions are set to no longer collide with the map
+				selected_item.collision_layer = 8
+				selected_item.collision_mask = 8
+				#call_deferred performs code when it can, so the rigidbody doesn't break. Other than that, I don't know
+				selected_item.freeze = true
+				#item is reparented to the available slot
+				selected_item.reparent(available_slot)
+				selected_item.call_deferred("set", "position", available_slot.get_child(0).position + Vector2(9,9))
+				selected_item.call_deferred("set", "rotation", 0)
 				lift_line.curve.set_point_position(1, Vector2(0, 41))
-				$Items.get_child(0).queue_free()
-			#EQUIP ITEM
-			elif $Items.get_child(0).equipable:
-				get_parent().equipped_item = $Items.get_child(0)
-				$Items.get_child(0).freeze = true
-				$Items.get_child(0).get_child(2).disabled = true
-				$Items.get_child(0).reparent(get_parent().get_node("Appearance/Top_Clothing"))
-				get_parent().get_node("Appearance/Top_Clothing").get_child(0).position = Vector2(0,0)
-				get_parent().get_node("Appearance/Top_Clothing").get_child(0).rotation = 45
-	
-	#HOLDING ITEM
-	elif event.is_action_pressed("Use") and selected_item != null:
-		#EAT ITEM
-		if selected_item.eat_heal_amount > 0:
-			get_parent().current_health += selected_item.eat_heal_amount
-			lift_line.curve.set_point_position(1, Vector2(0, 41))
-			selected_item.queue_free()
-		#EQUIP ITEM
-		elif selected_item.equipable:
-			get_parent().equipped_item = selected_item
-			selected_item.freeze = true
-			selected_item.get_child(2).disabled = true
-			selected_item.reparent(get_parent().get_node("Appearance/Top_Clothing"))
-			get_parent().get_node("Appearance/Top_Clothing").get_child(0).position = Vector2(0,0)
-			get_parent().get_node("Appearance/Top_Clothing").get_child(0).rotation = 45
-		selected_item = null
+				selected_item = null
+			#Drop item if there are no more slots
+			else:
+				selected_item.get_child(0).set_node_b(selected_item.get_path())
+				selected_item = null
+				lift_line.curve.set_point_position(1, Vector2(0, 41))
+			
+		##EAT ITEM
+		#if selected_item.eat_heal_amount > 0:
+			#get_parent().current_health += selected_item.eat_heal_amount
+			#lift_line.curve.set_point_position(1, Vector2(0, 41))
+			#selected_item.queue_free()
+			#selected_item = null
+		##EQUIP ITEM
+		#elif selected_item.equipable:
+			#get_parent().equipped_item = selected_item
+			#selected_item.freeze = true
+			#selected_item.get_child(2).disabled = true
+			#selected_item.reparent(get_parent().get_node("Appearance/Top_Clothing"))
+			#get_parent().get_node("Appearance/Top_Clothing").get_child(0).position = Vector2(0,0)
+			#get_parent().get_node("Appearance/Top_Clothing").get_child(0).rotation = 45
+			#selected_item = null
 
 
 #DRAW LIFT LINE
 func _draw() -> void:
+	#Grabbing item
 	if selected_item != null:
 		draw_polyline($Lift_Line.curve.get_baked_points(), Color(0.27,0.83,0.9,0.5), 1.5)
-	elif Input.is_action_pressed("Left_Click") and !$Pointer.get_colliding_bodies().is_empty():
-				draw_line(Vector2(0,41), line_of_sight_2.target_position + Vector2(4,32), Color(0.78,0,0.82,0.5), 1.5)
+	#Canonot grab item
+	elif Input.is_action_pressed("Left_Click"):
+		draw_line(line_of_sight_2.position, (line_of_sight_2.target_position - self.position) / 1.1, Color(0.78,0,0.82,0.5), 1.5)
 
 #DETECT WHAT YOU CLICK ON
 func find_colliding_item():
@@ -129,28 +140,18 @@ func find_colliding_item():
 	if !collided_things.is_empty():
 		return collided_things[0]
 
-#ITEM INSIDE ITEM FRAME
-func _on_item_detect_body_entered(body: Node2D) -> void:
+func _on_slot_0_selected() -> void:
+	if $Slots/Slot_0/Button/Drop.visible == false:
+		$Slots/Slot_0/Button/Drop.visible = true
+		$Slots/Slot_0/Button/Use.visible == true
 
-	#ITEM FALLS INTO ITEM FRAME
-	if selected_item == null and body.collectable:
-		#get item parent beforehand to help aleviate pressure on code
-		var item_collection : Node2D = $Items
-		#reset collision layers so the item doesn't collide with the world while in the frame
-		body.collision_layer = 8
-		body.collision_mask = 8
-		#freeze and reparent item to node declared above
-		#call_deferred performs code when it can, so the rigidbody doesn't break. Other than that, I don't know
-		body.call_deferred("set", "freeze", true)
-		body.call_deferred("reparent", item_collection)
+func _on_slot_1_selected() -> void:
+	pass # Replace with function body.
 
-	#HOVERING ITEM OVER ITEM FRAME
-	elif body.collectable:
-		#the item frame anticipates the item being dropped so it can catch it
-		stick_waiting = true
 
-#ITEM OUTSIDE ITEM FRAME
-func _on_item_detect_body_exited(_body) -> void:
-	#if the item left the frame, then...
-	#...the item frame does not anticipate an item being placed inside
-	stick_waiting = false
+func _on_slot_2_selected() -> void:
+	pass # Replace with function body.
+
+
+func _on_slot_3_selected() -> void:
+	pass # Replace with function body.
